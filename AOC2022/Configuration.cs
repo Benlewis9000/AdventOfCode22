9a00d89cd225389666ebb0a6d1b58b62a8e101b9
Ben Lewis <ben.lewis@willistowerswatson.com>
2022-12-07 01:41:50 +0000

    Basic Configuration

---
+++ AOC2022/Configuration.cs
@@ -0,0 +1,15 @@
+﻿namespace Aoc.Core;
+
+internal class Configuration
+{
+    public string BaseAddress { get; }
+    public string SessionId { get; }
+    public Problem Problem { get; }
+
+    private Configuration(string baseAddress, string sessionId, Problem problem)
+    {
+        BaseAddress = baseAddress;
+        SessionId = sessionId;
+        Problem = problem;
+    }
+}
