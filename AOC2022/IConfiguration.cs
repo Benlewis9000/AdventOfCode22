02ac8e7195e43bdbde370a2c196b35540a945b65
Ben Lewis <ben.lewis@willistowerswatson.com>
2022-12-07 01:43:08 +0000

    Configuration interface

---
+++ AOC2022/IConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Aoc.Core;
+
+internal interface IConfiguration
+{
+    public string GetAddress();
+    public string GetCookie();
+    public string GetYear();
+    public string GetDay();
+    public string GetPart();
+}
