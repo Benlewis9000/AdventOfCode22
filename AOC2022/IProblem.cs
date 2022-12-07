8eec63f3c98b82c80a2915269892ca98373772d8
Ben Lewis <ben.lewis@willistowerswatson.com>
2022-12-07 01:39:43 +0000

    Problem split into interface, checked and raw serializable types

---
+++ AOC2022/IProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Core;
+
+internal interface IProblem
+{
+    public int Year { get; }
+    public int Day { get; }
+    public int Part { get; }
+}
