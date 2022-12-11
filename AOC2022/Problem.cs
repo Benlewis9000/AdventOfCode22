6a37f3b099908c43c1f185ea30917dd6678decfc
Ben Lewis <ben.lewis@willistowerswatson.com>
2022-12-11 21:55:13 +0000

    Refactored CheckProblem to just Problem

--- AOC2022/Problem.cs
+++ AOC2022/Problem.cs
@@ -1,16 +1,48 @@
 ﻿namespace Aoc.Core;
 
-public struct Problem : IProblem
+public class Problem : IProblem
 {
     public int Year { get; }
     public int Day { get; }
     public int Part { get; }
 
-
     public Problem(int year, int day, int part)
     {
-        Year = year;
-        Day = day;
-        Part = part;
+        if (ValidateYear(year)) Year = year;
+        if (ValidateDay(day)) Day = day;
+        if (ValidatePart(part)) Part = part;
+    }
+
+    // Validation does not prevent future dates from being entered. This is handled by the InputFetcher.
+
+    private bool ValidateYear(int year)
+    {
+        if (year < 2015)
+        {
+            throw new ArgumentOutOfRangeException(nameof(year), $"problem year may not precede 2015, but was {year}");
+        }
+
+        return true;
+    }
+
+    private bool ValidateDay(int day)
+    {
+        if (day is < 0 or > 25)
+        {
+            throw new ArgumentOutOfRangeException(nameof(day),
+                $"problem day must be between 1 and 25 inclusive, but was {day}");
+        }
+
+        return true;
+    }
+
+    private bool ValidatePart(int part)
+    {
+        if (part is < 1 or > 2)
+        {
+            throw new ArgumentOutOfRangeException(nameof(part), $"problem part may only be 1 or 2, but was {part}");
+        }
+
+        return true;
     }
 }
