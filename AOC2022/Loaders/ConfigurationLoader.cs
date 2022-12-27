096d002af91a3f2f6c947ce97045510342a0c53d
Ben Lewis <ben.lewis@willistowerswatson.com>
2022-12-27 00:24:00 +0000

    Default path, empty ctr for deserializer

--- AOC2022/Loaders/ConfigurationLoader.cs
+++ AOC2022/Loaders/ConfigurationLoader.cs
@@ -1,13 +1,16 @@
 ﻿using Newtonsoft.Json;
-using System.IO;
-using System.Text.Json;
 
 namespace Aoc.Core.Loaders;
 
 public class ConfigurationLoader : ILoader<IConfiguration?>
 {
+    private const string cDefaultPath = @"config.json";
     private readonly string _path;
-    
+
+    public ConfigurationLoader() : this(cDefaultPath)
+    {
+    }
+
     public ConfigurationLoader(string path)
     {
         _path = path;
